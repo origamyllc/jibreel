79c2f7bfdfb5e8053959fb4cb074dcf160783b63
prashunjaveri@gmail.com <prashun@origamyinc.com>
2015-12-03 23:49:23 -0800

    cleaning  controller code

--- Mesh/nodes/jibreel.mesh.nodes.control.js
+++ Mesh/nodes/jibreel.mesh.nodes.control.js
@@ -15,27 +15,34 @@ module.exports = function() {
     self.redis= redis;
     self.lru =lru;
     self.stream();
-    self.count =0;
   }
 
   self.stream = function(){
     self.bus.on('stream',function(data){
-      ++ self.count;
-          if(self.count === 1) {
-            self.lru.get(self.node.fullName,function(adjacentNodes){
-              adjacentNodes.forEach(function(adjacentNode){
-                if(adjacentNode.split(".").indexOf("config") !== -1) {
-                  self.redis.get(adjacentNode, function (config) {
-                    console.log(config);
-                  });
-                }
-              })
+            self.getConfiguration(function(config){
+              console.log(data);
+              console.log(config);
             });
-          }
-        console.log(data);
       });
   }
 
+  self.getConfiguration =function(callback){
+
+    // todo configuration should be pulled only once
+
+      self.lru.get(self.node.fullName, function (adjacentNodes) {
+        adjacentNodes.forEach(function (adjacentNode) {
+          if (adjacentNode.split(".").indexOf("config") !== -1) {
+
+    //todo configuration has to come from lru instead of redis
+
+            self.redis.get(adjacentNode, function (config) {
+              callback(config);
+            });
+          }
+        });
+      });
+  }
 
   return new deviceControllerNode();
 
